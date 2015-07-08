139c5ee3178a76b65fec96662c136b2d246c367f
Pesegato <pesegato@outlook.com>
2015-07-08 14:08:54 +0200

    Added StringBuilder

---
+++ src/main/java/model/builders/StringBuilder.java
@@ -0,0 +1,31 @@
+package model.builders;
+
+import model.builders.definitions.DefElement;
+import model.builders.definitions.Definition;
+
+public class StringBuilder extends Builder {
+
+    private static final String STRING = "string";
+
+    String value;
+
+    public StringBuilder(Definition def) {
+        super(def);
+        for (DefElement de : def.getElements()) {
+            switch (de.name) {
+                case STRING:
+                    value = de.getVal();
+                    break;
+            }
+        }
+    }
+
+    public String build() {
+        return value;
+    }
+
+    @Override
+    public void readFinalizedLibrary() {
+    }
+
+}
