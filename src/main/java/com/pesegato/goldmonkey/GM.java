4df34212a85d2044082f98ef4fc522f989f26313
Pesegato <pesegato@outlook.com>
2015-07-08 12:19:25 +0200

    Added the GM ease class

---
+++ src/main/java/com/pesegato/goldmonkey/GM.java
@@ -0,0 +1,12 @@
+
+package com.pesegato.goldmonkey;
+
+import com.jme3.math.ColorRGBA;
+import model.builders.ColorBuilder;
+import model.builders.definitions.BuilderManager;
+
+public class GM {
+    public static ColorRGBA getColorRGBA(String id){
+        return ((ColorBuilder)BuilderManager.getBuilder("com.pesegato.goldmonkey.ColorBuilder",id, ColorBuilder.class)).buildColorRGBA();
+    }
+}
